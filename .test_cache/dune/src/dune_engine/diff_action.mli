93fc9a06bacb47e9a2a265541085e9e858f71c12
David Finson <davidfinson@microsoft.com>
2026-02-15 15:27:36 +0200

    swap dune for ocaml-re (smaller)

--- .test_cache/dune/src/dune_engine/diff_action.mli
+++
@@ -1,3 +0,0 @@
-open Import
-
-val exec : Loc.t -> (Path.t, Path.Build.t) Dune_util.Action.Diff.t -> unit Fiber.t
--- .test_cache/dune/src/dune_scheduler/dune_scheduler.ml
+++
@@ -1,7 +0,0 @@
-module Scheduler = Scheduler
-module Async_io = Async_io
-module File_watcher = File_watcher
-
-module For_benchmarks = struct
-  module Thread_pool = Thread_pool
-end
