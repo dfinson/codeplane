320c7359f6a772382302c1fdb465c5251e0e3c4c
David Finson <davidfinson@microsoft.com>
2026-03-08 12:27:10 +0200

    fix(infra): use AAD auth for storage provider

--- ranking/infra/main.tf
+++ ranking/infra/main.tf
@@ -11,7 +11,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
+  subscription_id            = var.subscription_id
+  storage_use_azuread        = true
 }
 
 variable "subscription_id" {
